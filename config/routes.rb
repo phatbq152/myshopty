e8373d5024aa44a7177589e787a5cdb3501650e8
phatbq152 <quangphatbui798@gmail.com>
2025-01-06 15:42:45 +0700

    up1

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get "products/index"
+  resources :products
+  root "products#index"
+  get "/products", to: "products#index"
+
+get "/products/new", to: "products#new"
+post "/products", to: "products#create"
+
+get "/products/:id", to: "products#show"
+
+get "/products/:id/edit", to: "products#edit"
+patch "/products/:id", to: "products#update"
+put "/products/:id", to: "products#update"
+
+delete "/products/:id", to: "products#destroy"
+
+end
